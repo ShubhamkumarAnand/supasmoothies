e840147508d4be95466c3ac8aa23769a4a2e3a05
imskanand <the.anand3099@gmail.com>
2023-01-03 22:07:44 +0530

    Smoothie Card complete

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,43 @@
+import { useState,useEffect } from 'react'
 import supabase from '../config/supabaseConfig'
+import SmoothieCard from '../components/SmoothieCard'
 
 const Home = () => {
-  console.log(supabase)
+  const [fetchError, setFetchError] = useState(null)
+  const [smoothies, setSmoothies] = useState(null)
+
+  useEffect(() => {
+    const fetchSmoothies = async () => {
+      const { data: read_smoothies, error } = await supabase
+        .from('smoothies')
+        .select('*')
+
+      if (error) {
+        setFetchError("Couldn't find smoothies")
+        setSmoothies(null)
+        console.error(error)
+      }
+      if (read_smoothies) {
+        setSmoothies(read_smoothies)
+        setFetchError(null)
+      }
+    }
+    fetchSmoothies()
+  }, [])
+
   return (
     <div className="page home">
-      <h2>Home</h2>
+      { fetchError && (<p>{ fetchError }</p>) }
+      { smoothies && (
+        <div className='smoothies'>
+          {/* Order By Buttons */}
+          <div className="smoothie-grid">
+            { smoothies.map(smoothie => (
+              <SmoothieCard key={ smoothie.id } smoothie={ smoothie } />
+            )) }
+          </div>
+        </div>
+      )}
     </div>
   )
 }
