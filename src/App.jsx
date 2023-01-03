ee9be944ca77ce602aea2ca4b8d5cd5b834524b9
imskanand <the.anand3099@gmail.com>
2023-01-03 19:34:49 +0530

    Starter Project Setup For the Supa Smoothie

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import './App.css'
+// pages
+import Home from "./pages/Home"
+import Create from "./pages/Create"
+import Update from "./pages/Update"
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function App(){
   return (
-    <div className="App">
-      <h1>Hello Supabase!</h1>
-    </div>
+    <BrowserRouter>
+      <nav>
+        <h1>Supa Smoothies</h1>
+        <Link to="/">Home</Link>
+        <Link to="/create">Create New Smoothie</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={ <Home /> } />
+        <Route path="/create" element={ <Create /> } />
+        <Route path="/:id" element={ <Update /> } />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
